934c327402eef0f0a4fc3fbd6e34e867e9062455
Albert <albertkaang@gmail.com>
2024-06-25 16:29:44 -0400

    seperated clothing and navbar menus

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Navbar from "./components/Navbar.jsx";
+import ClothingMenu from "./components/ClothingMenu.jsx";
 import Home from "./components/pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <Router>
       <Navbar />
+      <ClothingMenu />
       <Routes>
         <Route exact path="/" element={<Home />} />
       </Routes>
